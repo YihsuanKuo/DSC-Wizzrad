88a42c45be5e04e315697577532875776fa59c9a
Linus Lee <87669045+Linusdsnoif@users.noreply.github.com>
2025-04-06 12:12:10 -0700

    basketball

--- my-project/app/(src)/page.tsx
+++ my-project/app/(src)/page.tsx
@@ -2,8 +2,9 @@ import HomeContent from "@/components/HomeContent";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-[calc(100vh-4rem)] bg-#F2F4F7 p-3">
+    <div className="flex flex-col justify-center items-center h-[calc(100vh-4rem)] bg-[#F2F4F7] p-3">
       <HomeContent />
+      <img src={"/assets/Basketball.png"} alt="A nice image" />
     </div>
   );
 }
