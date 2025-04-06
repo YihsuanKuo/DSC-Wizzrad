b009abd980f9e5f67ac7b98429dda482ec6dbf60
Michael Luo <mluo.lion@gmail.com>
2025-04-06 12:16:41 -0700

    updated website

--- my-project/app/(src)/page.tsx
+++ my-project/app/(src)/page.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
 import HomeContent from "@/components/HomeContent";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-[calc(100vh-4rem)] bg-#F2F4F7 p-3">
+    <div className="flex flex-col justify-center items-center h-[calc(100vh-4rem)] bg-[#F2F4F7] p-3">
+      <Image
+        src="/assets/lebron.jpg"
+        alt="LeBron James"
+        width={300}
+        height={400}
+        className="mb-4 rounded-lg shadow-lg"
+      />
       <HomeContent />
     </div>
   );
--- my-project/next.config.ts
+++ my-project/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: [""], // ✅ Allow Sanity images
+    domains: ["images.unsplash.com"], // ✅ Allow Sanity images
   },
   experimental: {
     ppr: "incremental",
